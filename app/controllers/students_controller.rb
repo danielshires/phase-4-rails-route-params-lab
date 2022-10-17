3bd2996e5fd62a004e722252e148a43bbac3d4c1
danielshires <hello@danielshires.com>
2022-10-17 20:07:27 +1100

    Assignment complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
+
+    if params[:name]
+      students = Student.all.filter do |student| 
+       student[:first_name].downcase == params[:name].downcase || student[:last_name].downcase == params[:name].downcase
+      end
+      return render json: students
+    end
+
     students = Student.all
+    render json: students
+  end
+
+  def show
+    students = Student.find_by(id: params[:id])
     render json: students
   end
 
